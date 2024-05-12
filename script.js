5f213a12fe4012356af3fd9299c0e595b6d5b139
dharmbirroy <dharmbirroy.1@gmail.com>
2024-05-12 21:45:26 +0530

    third commit

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ function reload () {
 async function fetchNews(query){
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
-    console.log(data)
+    // console.log(data)
     bindData(data.articles);
 }
 
 function bindData (articles){
-    const cardsContainer = document.getElementById('cards-container');
-    const newsCardTemplate = document.getElementById('template-news-card');
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
 
-    cardsContainer.innerHTML = '';
+    cardsContainer.innerHTML = "";
 
-    articles.forEach(article =>{
+    articles.forEach((article) =>{
         if(!article.urlToImage) return; 
         const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article); 
@@ -29,10 +29,10 @@ function bindData (articles){
 }
 
 function fillDataInCard(cardClone, article){
-    const newsImg = cardClone.querySelector('#news-img');
-    const newsTitle = cardClone.querySelector('#news-title');
-    const newsSource = cardClone.querySelector('#news-source');
-    const newsDesc = cardClone.querySelector('#news-desc');
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
 
     newsImg.src = article.urlToImage;
     newsTitle.innerHTML = article.title;
@@ -59,8 +59,8 @@ function onNaveItemClick(id) {
 
 }
 
-const searchButton = document.getElementById('search-button');
-const searchText = document.getElementById('search-text');
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
 
 searchButton.addEventListener("click", ()=>{
     const query = searchText.value; 
